73fa532de77ed7a8efe2dbdbb817d45234d46dff
Tyler Reece <tyler_reece@outlook.com>
2019-01-16 20:16:52 -0500

    Set up starting point for the rest of the website

--- src/App.js
+++ src/App.js
@@ -2,6 +2,19 @@ import React, {Component} from 'react';
 import './App.css';
 import {createMuiTheme} from '@material-ui/core/styles';
 import {MuiThemeProvider} from "@material-ui/core";
+import { createBrowserHistory } from "history";
+import { Router, Route, Switch } from "react-router-dom";
+import indexRoutes from "./routes/index.jsx";
+import "./assets/scss/material-kit-react.css?v=1.3.0";
+
+// @fontawesome
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faIgloo } from '@fortawesome/free-solid-svg-icons';
+import { fab } from '@fortawesome/free-brands-svg-icons';
+
+library.add(faIgloo, fab);
+
+const hist = createBrowserHistory();
 
 const theme = createMuiTheme({
     palette: {
@@ -13,8 +26,14 @@ class App extends Component {
     render() {
         return (
             <MuiThemeProvider theme={theme}>
-                <div className="App">
-                </div>
+                <Router history={hist}>
+                    <Switch>
+                        {indexRoutes.map((prop, key) => {
+                            return <Route path={prop.path} key={key} component={prop.component} />;
+                        })}
+                    </Switch>
+                </Router>,
+                {/*<ProfilePage/>*/}
             </MuiThemeProvider>
         );
     }
---
+++ src/routes/index.jsx
@@ -0,0 +1,13 @@
+// import Components from "views/Components/Components.jsx";
+// import LandingPage from "views/LandingPage/LandingPage.jsx";
+import ProfilePage from "../ProfilePage.jsx";
+// import LoginPage from "views/LoginPage/LoginPage.jsx";
+
+var indexRoutes = [
+  // { path: "/landing-page", name: "LandingPage", component: LandingPage },
+  { path: "/", name: "ProfilePage", component: ProfilePage },
+  // { path: "/login-page", name: "LoginPage", component: LoginPage },
+  // { path: "/", name: "Components", component: Components }
+];
+
+export default indexRoutes;
