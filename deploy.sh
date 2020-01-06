b81512ac43fdb21b8dd838d6c011f31837709a54
Tyler Reece <tyler_reece@outlook.com>
2020-01-05 21:57:11 -0500

    Small updates to deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,13 +1,10 @@
 yarn deploy
-git checkout gh-pages
-git pull
-rm -rf build node_modules .idea
-mkdir tylerreece
-mv ./static ./tylerreece
-mv ./favicon.ico ./tylerreece
-mv ./manifest.json ./tylerreece
-touch CNAME
-echo tylerreece.tech > CNAME
-git add .
+git checkout gh-pages && git pull
+rm -rf build
+mkdir tylerreece && mv static favicon.ico manifest.json ./tylerreece
+touch CNAME && echo tylerreece.tech > CNAME
+git add tylerreece/ static/ favicon.ico manifest.json
 git commit -m "Moved files to ./tylerreece"
 git push
+git reset --hard
+git checkout master
