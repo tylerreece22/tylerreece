ca02c115c2152849059c22d622ca75225ecbe717
Tyler Reece <tyler_reece@outlook.com>
2020-01-05 22:02:23 -0500

    Cleaned up deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,22 @@
+echo --- Deploying to gh-pages ---
 yarn deploy
-git checkout gh-pages && git pull
+
+echo --- Checking out gh-pages ---
+git checkout gh-pages
+git pull
+
+echo --- Moving files to ./tylerreece ---
 rm -rf build
-mkdir tylerreece && mv static favicon.ico manifest.json ./tylerreece
-touch CNAME && echo tylerreece.tech > CNAME
+mkdir tylerreece
+mv static favicon.ico manifest.json ./tylerreece
+
+echo --- Adding CNAME ---
+touch CNAME
+echo tylerreece.tech > CNAME
 git add tylerreece/ static/ favicon.ico manifest.json
 git commit -m "Moved files to ./tylerreece"
+
+echo --- Pushing new files to gh-pages ---
 git push
 git reset --hard
 git checkout master
